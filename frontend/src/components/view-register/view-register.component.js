95234b59d061adf08fa86a47920202c934ebf805
alinaciuysal <alinaciuysal@gmail.com>
2017-06-16 23:32:42 +0200

    second phase of registration pages

--- frontend/src/components/view-register/view-register.component.js
+++ frontend/src/components/view-register/view-register.component.js
@@ -32,16 +32,54 @@ class ViewRegisterComponentController{
     }
 
     submitUserRegistrationRequest(){
-        let email = this.login.email;
-        let password = this.login.password;
+        let email = this.userRegister.email;
+        let password = this.userRegister.password;
 
-        // TODO: add other attributes for different registration types
-
-        this.UserService.register(email,password).then(()=> {
+        console.log(email + " " + password);
+        this.UserService.register(email, password).then(()=> {
             // TODO: application logic goes here after submitting registration request
             this.$state.go('mainPage',{});
         });
     }
+
+    submitDelivererRegistrationRequest() {
+        let name = this.delivererRegister.name;
+        let password = this.delivererRegister.password;
+        let surname = this.delivererRegister.surname;
+        let email = this.delivererRegister.email;
+        let birthday = this.delivererRegister.birthday;
+        let phoneNumber = this.delivererRegister.phoneNumber;
+        let maxWeight = this.delivererRegister.maxWeight;
+
+        // Ref: https://stackoverflow.com/questions/5416920/timestamp-to-human-readable-format
+        let date = new Date(birthday);
+        console.log("date: " + date);
+
+        var date = new Date(timestamp).getDate();
+        var month = new Date(timestamp).getMonth()+1;
+        var year = new Date(timestamp).getFullYear();
+        var original_date =  date + '-' + month+ '-' + year;
+        console.log(original_date);
+
+        // https://stackoverflow.com/questions/43277458/how-to-specify-timestamp-format-when-converting-to-human-readable-string-in-js
+        // https://momentjs.com/
+
+        console.log(name + " " + password + " " + surname + " " + email + " " + date + " " + phoneNumber + " " + maxWeight);
+
+        // TODO: API CALLS
+    }
+
+    submitShopRegistrationRequest() {
+        let name = this.shopRegisterer.name;
+        let address = this.shopRegisterer.address;
+        let email = this.shopRegisterer.email;
+        let password = this.shopRegisterer.password;
+        let phoneNumber = this.shopRegisterer.phoneNumber;
+        console.log(name + " " + address + " " + email + " " + password + " " + phoneNumber);
+
+        // TODO: API CALLS
+    }
+
 
     static get $inject(){
         return ['$state', UserService.name];
