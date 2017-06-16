bfeba79aa467a98c4e670d5f1136f08a522536ac
Gulce Basar <gulcebasar@gmail.com>
2017-06-16 16:12:36 +0200

    main page carousel (not working!)

--- frontend/src/components/app-content/app-content.js
+++ frontend/src/components/app-content/app-content.js
@@ -6,6 +6,7 @@ import AppHeader from './../app-header/app-header';
 import AppFooter from './../app-footer/app-footer';
 import LeftMenu from './../left-menu/left-menu';
 
+
 import AppContentComponent from './app-content.component';
 
 export default angular.module('AppView', [
