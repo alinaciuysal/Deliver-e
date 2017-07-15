bd84854c9f7a0812d8205f0bab2eac78c2f17219
Gulce Basar <gulcebasar@gmail.com>
2017-07-15 18:42:58 +0200

    fix mistake

--- backend/user/userSchema.js
+++ backend/user/userSchema.js
@@ -90,7 +90,7 @@ userSchema.pre('save', function(next) {
 
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    compare(candidatePassword, this.password, function(err, isMatch) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
